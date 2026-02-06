678028e7673c7e3181cbabe1f8afd4a0447e3de7
nocdn <bartek.bak@protonmail.com>
2026-02-06 20:25:58 +0000

    fix: builds failing due to google fonts network request for og image

--- app/opengraph-image.tsx
+++ app/opengraph-image.tsx
@@ -6,27 +6,12 @@ export const alt = "Bartosz Bak - Design Engineer"
 export const size = { width: 1200, height: 630 }
 export const contentType = "image/png"
 
-async function loadGoogleFont(font: string, text: string, weight: number = 400) {
-  const url = `https://fonts.googleapis.com/css2?family=${font}:wght@${weight}&text=${encodeURIComponent(text)}`
-  const css = await (await fetch(url)).text()
-  const resource = css.match(/src: url\((.+)\) format\('(opentype|truetype)'\)/)
-
-  if (resource) {
-    const response = await fetch(resource[1])
-    if (response.status === 200) {
-      return await response.arrayBuffer()
-    }
-  }
-
-  throw new Error("Failed to load font data")
-}
-
 export default async function Image() {
   const nameText = "Bartosz Bak"
   const titleText = "Design Engineer"
 
   const [interSemiBold, openRundeMedium] = await Promise.all([
-    loadGoogleFont("Inter", nameText, 600),
+    readFile(join(process.cwd(), "app/fonts/Inter-SemiBold.ttf")),
     readFile(join(process.cwd(), "app/fonts/OpenRunde-Medium.otf")),
   ])
 
